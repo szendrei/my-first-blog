c29fec635d33d392d93a901f56190b468ab1907f
szendrei <szendrgigi@gmail.com>
2015-10-21 23:42:21 +0200

    Added comment model and views, comment approve, remove and user authentication.

---
+++ blog/migrations/0003_auto_20151021_2215.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_comment'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='comment',
+            name='approved_comment',
+            field=models.BooleanField(default=False),
+        ),
+    ]
